7feef3576759f810b3c17c3e95fa7e1daba0d6ad
L3yzs <zlxcaz14@gmail.com>
2026-03-27 00:49:45 +0300

    Delete hypr directory

--- hypr/scripts/back-to-static.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# 1. Önce videoyu (mpvpaper) anında kapat 🛑
-pkill mpvpaper || true
-
-# 2. Senin asıl wallpaper scriptini çalıştır 🖼️
-# (Yolu tam senin dediğin gibi ayarladım)
-/home/leyzs/.config/hypr/scripts/wallpapers/set-wallpaper.shk
--- hypr/shaders/switch_shader.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-if [ "$1" == "off" ]; then
-    hyprctl keyword decoration:screen_shader ~/.config/hypr/shaders/off.glsl
-else
-    # Şifreyi girdiğin an burası çalışacak
-    hyprctl keyword decoration:screen_shader ~/.config/hypr/shaders/vibrance.glsl &
-fi
